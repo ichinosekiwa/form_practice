c387b28840bf72c8cbc4dd004f57eff9f16d8eb9
ichinosekiwa <128582009+ichinosekiwa@users.noreply.github.com>
2023-11-03 12:05:22 +0900

    送信時リロードされる問題を修正

--- js/form.js
+++ js/form.js
@@ -1,7 +1,7 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const contactForm = document.querySelector('.contact');
+  const contactForm = document.getElementById('contact');
   if (contactForm) {
     const errorText = 'error';
     const telItem = document.querySelectorAll('.tel');
@@ -19,44 +19,59 @@ document.addEventListener('DOMContentLoaded', () => {
     };
     //送信したらエラーを表示する全ての要素を削除（初期化）
     contactForm.addEventListener('submit', (e) => {
+      // リロードを防ぐ？
+      e.preventDefault();
       const errorItems = contactForm.querySelectorAll('.' + errorText);
       errorItems.forEach((item) => {
         item.remove();
       });
+      // バリデーションが成功したかどうか (constはダメ)
+      let validate = true;
 
       // 電話番号についての検証
       telItem.forEach((item) => {
         const pattern = /^[0-9\-]+$/;
         if (item.value !== '') {
-          //test() メソッドで値を判定 NGならメッセージを表示してフォームの送信を中止する
+          // test() メソッドで値を判定 NGならメッセージを表示してフォームの送信を中止する
           if (!pattern.test(item.value)) {
-            createError(item, '半角数字とハイフンのみ使用できます。');
-            e.preventDefault();
+            createError(item, '半角数字とハイフンのみ使用できます');
+            validate = false;
           }
         }
       });
+
       // 年齢についての検証
       ageItem.forEach((item) => {
         const pattern = /^[0-9]+$/;
         if (item.value !== '') {
           if (!pattern.test(item.value)) {
-            createError(item, '半角数字のみ使用できます。');
-            e.preventDefault();
+            createError(item, '半角数字のみ使用できます');
+            validate = false;
           }
         }
       });
+
+      if (validate) {
+        const nameInput = document.querySelector('.name');
+        const telInput = document.querySelector('.tel');
+        const ageInput = document.querySelector('.age');
+        const messageInput = document.querySelector('.message');
+
+        const name = nameInput.value;
+        const tel = telInput.value;
+        const age = ageInput.value;
+        const message = messageInput.value;
+
+        // 入力内容を画面に出力
+        document.getElementById('result').textContent = `
+          名前: ${name}
+          電話番号: ${tel}
+          年齢: ${age}
+          お問い合わせ内容: ${message}
+        `;
+      } else {
+        e.preventDefault();
+      }
     });
-  }
-  // バリデーションOKなら入力内容表示
-  const resultItem = document.getElementById('result');
-  if (errors.length === 0) {
-    result.innerHTML = `
-                        <p>入力内容を確認</p>
-                        <p>名前: ${name}</p>
-                        <p>電話番号: ${tel}</p>
-                        <p>年齢: ${age}</p>
-                        <p>問い合わせ内容: ${message}</p>
-                    `;
-    document.getElementById('contact').reset();
   }
 });
